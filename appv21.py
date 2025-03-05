0ca0997be0129c00c87a193426d94998df56a7c9
krishna90520 <krishna45teja@gmail.com>
2025-03-05 18:51:21 +0530

    Update appv21.py

--- appv21.py
+++ appv21.py
@@ -16,7 +16,7 @@ os.environ["STREAMLIT_SERVER_ENABLE_WATCHER"] = "false"  # Disable problematic w
 # Mapping of crop to the corresponding model file path
 crop_model_mapping = {
     "Paddy": "https://github.com/krishna90520/crop_/raw/refs/heads/main/classification_4Disease_best.pt",  # Replace with actual model URL
-    "Cotton": "https://your-storage-location/re_do_cotton_2best.pt",  # Replace with actual model URL
+    "Cotton": "https://github.com/krishna90520/crop_/raw/refs/heads/main/re_do_cotton_2best.pt",  # Replace with actual model URL
     "Groundnut": "https://your-storage-location/groundnut_best.pt"  # Replace with actual model URL
 }
 
